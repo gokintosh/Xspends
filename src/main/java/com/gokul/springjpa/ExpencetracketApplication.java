43210a7dc34c213d218260287faed1c3b275f540
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-22 20:39:23 +0100

    initial commit

---
+++ src/main/java/com/gokul/springjpa/ExpencetracketApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpencetracketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpencetracketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokul/springjpa/ExpencetracketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpencetracketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
